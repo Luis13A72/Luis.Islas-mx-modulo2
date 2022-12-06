e8ac1f04ccd8069232faa6030cc5930e7363cdbc
Luis13A72 <82126631+Luis13A72@users.noreply.github.com>
2022-12-06 10:12:06 -0600

    Class 1
    Jump & Run

---
+++ dino_runner/components/dinosaur.py
@@ -0,0 +1,52 @@
+import pygame
+from dino_runner.utils.constants import RUNNING, JUMPING #Se importa la constante a usar
+class Dinosaur:
+    def __init__(self): #Siempre ddebe estar para cuando se agregue un objeto al juego
+        self.image = RUNNING[0] #Al ser una lita inicia en posicion 0
+        self.dino_rect = self.image.get_rect()   #Coordenadas que tendra nuestra imagen, dando todos su parametros
+        self.dino_rect.x =80    #Dibujar donde se imprime el dinosaurio
+        self.dino_rect.y =310   #Dibujar donde se imprime el dinosaurio
+        self.step_index = 0    #Atributo de los pasos del dinosaurio
+
+        self.dino_run = True     #//Se crean dos variables mas
+        self.dino_jump = False   #//Se crean dos variables mas
+        self.jump_vel = 8.5 #//Se creala variable de velocidad de salto
+
+        
+        
+    def update(self, user_input):   #Siempre ddebe estar para cuando se agregue un objeto al juego
+
+        if self.dino_run:
+            self.run()  #Si está en Run se manda a llamar al metodo run
+        elif self.dino_jump:
+            self.jump() #Si está en Run se manda a llamar al metodo jump
+
+        if user_input[pygame.K_UP] and not self.dino_jump: #Entrada del usuario por medio de pygame para el comando arriba
+            self.dino_jump = True
+            self.dino_run = False
+       
+        elif not self.dino_jump:    #Si no está saltando entonces está corriendo
+            self.dino_jump = False
+            self.dino_run = True
+        
+        if self.step_index >= 10: #se debe resetear los steps para reiniciar la animación
+            self.step_index = 0  
+
+    def jump(self):
+        self.image = JUMPING    
+        self.dino_rect.y -= self.jump_vel*4
+        self.jump_vel -= 0.8
+
+        if self.jump_vel < -8.5:
+            self.dino_rect.y = 310
+            self.dino_jump = False
+            self.jump_vel = 8.5
+
+    def run(self):
+        self.image = RUNNING[0] if self.step_index < 5 else RUNNING[1]  #Operación ternaria, operación que pone un If y un Else, en una sola linea
+        self.dino_rect.x =80    #Dibujar donde se imprime el dinosaurio
+        self.dino_rect.y =310   #Dibujar donde se imprime el dinosaurio
+        self.step_index += 1   #Los Step avanzaran de unoen uno hasta llegar  a 5        
+            
+    def draw(self, screen):     #Siempre ddebe estar para cuando se agregue un objeto al juego, donde lo dibujara
+        screen.blit(self.image, (self.dino_rect.x, self.dino_rect.y)) # Se le pasa la imagen y la ubicación
--- dino_runner/components/game.py
+++ dino_runner/components/game.py
@@ -1,19 +1,20 @@
 import pygame
-
 from dino_runner.utils.constants import BG, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS
-
+from dino_runner.components.dinosaur import Dinosaur #se importa nuestra clase
 
 class Game:
     def __init__(self):
         pygame.init()
-        pygame.display.set_caption(TITLE)
-        pygame.display.set_icon(ICON)
-        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-        self.clock = pygame.time.Clock()
-        self.playing = False
-        self.game_speed = 20
-        self.x_pos_bg = 0
-        self.y_pos_bg = 380
+        pygame.display.set_caption(TITLE)   #Muestra el titulo que se presenta
+        pygame.display.set_icon(ICON)       #Coloca una imagen
+        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))    #Define el tamaño de la pantalla del juego
+        self.clock = pygame.time.Clock()    #Define los tiempos del juego
+        self.playing = False    
+        self.game_speed = 20    #20 posiciones que se irán cambiando
+        self.x_pos_bg = 0       #pocision en X
+        self.y_pos_bg = 380     #posición en Y
+
+        self.player = Dinosaur() #/Es la instancia de la clase Dinosaur
 
     def run(self):
         # Game loop: events - update - draw
@@ -25,25 +26,27 @@ class Game:
         pygame.quit()
 
     def events(self):
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                self.playing = False
+        for event in pygame.event.get():    #Lista de eventos del teclado
+            if event.type == pygame.QUIT:   #La funcion del tache para cerrar la ventana
+                self.playing = False        #Saca del juego
 
     def update(self):
-        pass
+        user_input = pygame.key.get_pressed()    #Todos los que se han presionado
+        self.player.update(user_input)    #/Llama al metodo que está en la clase de la instancia
 
     def draw(self):
-        self.clock.tick(FPS)
-        self.screen.fill((255, 255, 255))
-        self.draw_background()
-        pygame.display.update()
-        pygame.display.flip()
+        self.clock.tick(FPS)        #El tick dice en cada segundo cuantos frames se actualizan
+        self.screen.fill((255, 255, 255))   #Color que se pintará la pantalla  
+        self.draw_background()      #llama al metodo del fondo de pantalla
+        self.player.draw(self.screen)          #/Pasa los datos del screen, para dibujar al dinosaur
+        pygame.display.update()     #Actualiza un objeto en praticular
+        pygame.display.flip()   #Actualiza todos los objetos
 
     def draw_background(self):
-        image_width = BG.get_width()
-        self.screen.blit(BG, (self.x_pos_bg, self.y_pos_bg))
-        self.screen.blit(BG, (image_width + self.x_pos_bg, self.y_pos_bg))
-        if self.x_pos_bg <= -image_width:
+        image_width = BG.get_width()    #El ancho de la imagen del fondo
+        self.screen.blit(BG, (self.x_pos_bg, self.y_pos_bg))    #El Blit es para avisarle que se pondra un objeto en pantalla, para después mostrar 
+        self.screen.blit(BG, (image_width + self.x_pos_bg, self.y_pos_bg)) #BG: imagen  y luego las coordenadas
+        if self.x_pos_bg <= -image_width:       #Cuando llegué al limite creará otra imagen
             self.screen.blit(BG, (image_width + self.x_pos_bg, self.y_pos_bg))
             self.x_pos_bg = 0
-        self.x_pos_bg -= self.game_speed
+        self.x_pos_bg -= self.game_speed    #Disminuira a la velocidad que se le haya colocado
